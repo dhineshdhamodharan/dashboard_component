e8de1ffb5a251c2f87b45adb76005d11689db532
dhineshdhamodharan <83341500+dhineshdhamodharan@users.noreply.github.com>
2021-08-16 11:51:04 +0530

    Update sidemenu.js

--- src/sidemenu.js
+++ src/sidemenu.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react";
 function Sidemenu(){
     return(
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
